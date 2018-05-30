381eddfeaabc693955642b7a423c709f8ed825d7
twcardenas <tcardena@uncc.edu>
2018-05-30 07:43:57 -0400

    Updated

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, g, request, send_from_directory, abort, request_started
-from flask_restful import Resource, reqparse
+from flask import Flask, g, request
+from flask_restful import Resource
 import os
 from flask_restful_swagger_2 import Api, swagger, Schema
-
+from flask_cors import CORS
 from neo4j.v1 import GraphDatabase
 from neo4j.util import watch
 import logging
 from sys import stdout
-
+#MATCH(name: Name) RETURN DISTINCT name.surname, count(*) ORDER BY count(*) DESC
 
 def add_person(tx, id, given, surn, sex):
     tx.run("CREATE (a:Person {id: $id, sex:$sex}) "
@@ -40,6 +40,10 @@ def peopleWithoutParents(tx):
     for record in  tx.run("MATCH (a:Person)-[:NAME_IS]->(b) WHERE NOT (a)-[:CHILD_OF]->() return a, b"):
         print(record["a"]["id"], record["a"]["sex"], record["b"]["surname"], ',', record["b"]["given"])
 
+def uniqueSurnames(tx):
+    for record in  tx.run("MATCH(name: Name) RETURN DISTINCT name.surname ORDER BY name.surname"):
+        print(record['name.surname'])
+
 def peopleWithoutPartners(tx):
     for record in  tx.run("MATCH (a:Person)-[:NAME_IS]->(b) WHERE NOT (a)-[:PARTNER_OF]->() return a, b"):
         print(record["a"]["id"], record["a"]["sex"], record["b"]["surname"], ',', record["b"]["given"])
@@ -48,12 +52,13 @@ def peopleWithoutPartnersBySurname(tx, surname):
            surname=surname):
         print(record["a"]["id"], record["a"]["sex"], record["b"]["surname"], ',', record["b"]["given"])
 
-driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "gedcom"))
+driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "PASSWORD"))
 watch("neo4j.bolt", logging.DEBUG, stdout)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret guy'
 api = Api(app, title='Bloodline API', api_version='0.0.1')
+cors = CORS(app, resources={"/api/*": {"origins": "*"}})
 
 def get_db():
     if not hasattr(g, 'gedcom'):
@@ -73,6 +78,10 @@ def serialize_person(person):
     return {
         'id': person['id'],
         'sex': person['sex']
+        # ,
+        # 'names': {
+        #     'surname': names['surname']
+        # }
     }
 
 class PersonModel(Schema):
@@ -85,6 +94,41 @@ class PersonModel(Schema):
             'type': 'string',
         }
     }
+class NameModel(Schema):
+    type = 'object'
+    properties = {
+        'given': {
+            'type': 'string',
+        },
+        'surname': {
+            'type': 'string',
+        }
+    }
+class ParentlessPeopleList(Resource):
+    @swagger.doc({
+        'tags': ['people'],
+        'summary': 'Find people with no parents',
+        'description': 'Returns a list of people that do not have parents',
+        'responses': {
+            '200': {
+                'description': 'A list of people with no parents',
+                'schema': {
+                    'type': 'array',
+                    'items': PersonModel,
+                }
+            }
+        }
+    })
+    def get(self):
+        db = get_db()
+
+        results = db.run(
+            '''
+            "MATCH (a:Person) WHERE NOT (a)-[:CHILD_OF]->() return a"
+
+            '''
+        )
+        return [serialize_person(record['a']) for record in results]
 
 class PersonList(Resource):
     @swagger.doc({
@@ -102,7 +146,20 @@ class PersonList(Resource):
         }
     })
     def get(self):
+        token = request.headers.get('authorization')
+        print('get')
+        print(token)
+        import jwt
+        try:
+            payload = jwt.decode(token, 'kpTxN=)7mX3W3SEJ58Ubt8-')
+            print( payload['sub'])
+        except jwt.ExpiredSignatureError:
+            print('Signature expired. Please log in again.')
+        except jwt.InvalidTokenError:
+            print('Invalid token. Please log in again.')
+
         db = get_db()
+
         results = db.run(
             '''
             MATCH (a:Person) return a
@@ -110,7 +167,6 @@ class PersonList(Resource):
             '''
         )
         return [serialize_person(record['a']) for record in results]
-
 
 def importGEDCOM(file):
     with driver.session() as session:
@@ -152,6 +208,7 @@ def importGEDCOM(file):
                         session.write_transaction(add_child, id, childID)
 
 api.add_resource(PersonList, '/api/v0/people')
+api.add_resource(ParentlessPeopleList, '/api/v0/parentlesspeople')
 
 if __name__ == '__main__':
     app.debug = True
@@ -159,5 +216,3 @@ if __name__ == '__main__':
     host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get('PORT', 8081))
     app.run(host=host, port=port)
-
-#driver.session().read_transaction(peopleWithoutParentsBySurname, "Starnes")
